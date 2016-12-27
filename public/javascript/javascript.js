0aa37d42170da1db327a804568c82a3f559d9572
Eliezer Wohl <eliezerwohl@gmail.com>
2016-12-27 14:43:00 -0500

    jsToCss

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,35 +1,22 @@
-var a = $(".elieNav").offset().top;
+
 $(document).scroll(function(){
-    if($(this).scrollTop() > 0)
-    {   
-       $('.elieNav').css({"background":"white"});
-         $('.elieNav').css({"border-bottom":"1px solid #808080"});
-      } else {
-       $('.elieNav').css({"border-bottom":"none"});
-         $('.elieNav').css({"background":"transparent"});
-
-    }
+  if($(this).scrollTop() > 0)
+  {   
+    $('.elieNav').removeClass("topNav");
+    $('.elieNav').addClass("scrollTop");
+    } else { 
+      $('.elieNav').removeClass("scrollTop");
+      $('.elieNav').addClass("topNav");
+  }
 }); 
-
- $(".collapse").on('show.bs.collapse', function(){
-        $('.container-fluid').css({"background":"white"});
-    });
-    $(".collapse").on('hide.bs.collapse', function(){
-        $('.container-fluid').css({"background":"none"});
-    });
-
 
 $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
 
-
-
 var h = window.innerHeight;
 $(".welcomeBackground").css({"height":h})
-
-
 
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
