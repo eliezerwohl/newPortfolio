62c60568d4a38b457db3164e248add033982eb04
Eliezer Wohl <eliezerwohl@gmail.com>
2016-12-28 11:27:21 -0500

    hk

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var compression = require('compression');
+var minify = require('express-minify');
+app.use(compression());
+var logger = require('morgan');
+app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+app.get("/", function(req, res) {
+	res.sendFile(process.cwd() + "/public/views/home.html")
+})
+var port = process.env.PORT || 9000;
+app.listen(port, function() {
+	console.log("listening on port:" + port);
+});
