46b75373007fb46b9a7df669823d2be8403f7581
Eliezer Wohl <eliezerwohl@gmail.com>
2016-12-29 11:30:42 -0500

    made social and contact links and pictures change opacity on hover,  also cleaned up js and css files

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,35 +1,30 @@
 var compensate = 0
 
 function layoutFunction(){
-var h = window.innerHeight;
-$(".welcomeBackground").css({"height":h})
-
-if (window.innerWidth <= 767){
-  compensate  = 50
-}
-else{
-  compensate = 70
-}
-//on size change , if it's bigger than breaking point var 50 else 70
-$(".container").css({"min-height":h - compensate})
-
+	var h = window.innerHeight;
+	$(".welcomeBackground").css({"height":h})
+	if (window.innerWidth <= 767){
+  	compensate  = 50
+	}
+	else{
+  	compensate = 70
+	}
+	$(".container").css({"min-height":h - compensate})
 }
 layoutFunction()
-//run this on open and screen change
-
-  $(".navbar-toggle, .nav  .navbar-collapse a").on("click", function(e) {
-  	$("#bs-example-navbar-collapse-1").slideToggle("fast");
-  });
-  $(".navbar-brand").on("click", function() {
-    $("#bs-example-navbar-collapse-1").slideUp("fast");
-  })
 
 
+$(".navbar-toggle, .nav  .navbar-collapse a").on("click", function(e) {
+	$("#bs-example-navbar-collapse-1").slideToggle("fast");
+});
+$(".navbar-brand").on("click", function() {
+  $("#bs-example-navbar-collapse-1").slideUp("fast");
+})
 
 $( document ).ready(function() {
   $( window ).resize(function() {
-  layoutFunction()
-});
+ 	  layoutFunction()
+	});
 
   $(document).scroll(function(){
   if($(this).scrollTop() > 0){   
@@ -41,26 +36,23 @@ $( document ).ready(function() {
       $(".navbar-brand").removeClass("brandShow").addClass("brandHide")
   	}
 	});
-    $(".nav a").on("click", function(){
-      $(".nav").find(".active").removeClass("active");
-      $(this).parent().addClass("active");
-    });
-    $('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      
-      $('html, body').animate({
-        scrollTop: target.offset().top - 70
-      }, 1000);
-  
-      return false;
-    }
+  $(".nav a").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+   });
+   $('a[href*="#"]:not([href="#"])').click(function() {
+  	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		  if (target.length) {
+		    $('html, body').animate({
+		      scrollTop: target.offset().top - 70
+		    }, 1000);
+		    return false;
+		  }
+		}
 
-  }
-
-})
+	})
 }); 
 
 
