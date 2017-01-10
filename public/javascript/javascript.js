3f4892631819b39611679a9acb86c26df4210356
eliezerwohl <eliezerwohl@gmail.com>
2017-01-09 21:50:48 -0500

    greeting is fixed so navbar isn't selected when user is above 'myStory'

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,6 +1,16 @@
 var menuVar;
 var compensate = 0;
-var bodyScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+function scrollFunction(){
+  if($(this).scrollTop() > 0){   
+      $('.elieNav').removeClass("topNav").addClass("scrollNav");   
+      $(".navbar-brand").removeClass("brandHide").addClass("brandShow");
+     } 
+  else { 
+    $('.elieNav').removeClass("scrollNav").addClass("topNav");
+    $(".navbar-brand").removeClass("brandShow").addClass("brandHide");
+    $(".nav").find(".active").removeClass("active");
+  }
+}
 function menusScroll (data){
 if (((document.documentElement.scrollTop||document.body.scrollTop) >= data.offsetTop - 80)
 	&& ((document.documentElement.scrollTop||document.body.scrollTop) <=  data.offsetTop + data.clientHeight - 80)){
@@ -9,20 +19,10 @@ if (((document.documentElement.scrollTop||document.body.scrollTop) >= data.offse
 			menuVar = data.id;
 			menuVar += "Button";
     	$("#" + menuVar).parent().addClass("active");
+      console.log(menuVar)
 		}
 	}
-  // if ((document.body.scrollTop >= data.offsetTop - 80)
-  // && (document.body.scrollTop <=  data.offsetTop + data.clientHeight - 80)){
-  // if (menuVar != data.id) {
-  //   $(".nav").find(".active").removeClass("active");
-  //     menuVar = data.id;
-  //     menuVar += "Button";
-  //     $("#" + menuVar).parent().addClass("active");
-  //   }
-  // }
 }
-
-document.documentElement.scrollTop
 function layoutFunction(){
 	var h = window.innerHeight;
 	$(".welcomeBackground").css({"height":h});
@@ -35,7 +35,6 @@ function layoutFunction(){
 	$(".container").css({"min-height":h - compensate});
 }
 layoutFunction();
-
 $(".navbar-toggle, .nav  .navbar-collapse a").on("click", function(e) {
 	$("#bs-example-navbar-collapse-1").slideToggle("fast");
 });
@@ -43,32 +42,24 @@ $(".navbar-brand").on("click", function() {
   $("#bs-example-navbar-collapse-1").slideUp("fast");
 });
 $( document ).ready(function() {
+  scrollFunction()
 	var music = document.getElementById("music");
 	var myStory = document.getElementById("myStory");
 	var social = document.getElementById("social");
 	var contact = document.getElementById("contact");
 	var portfolio = document.getElementById("portfolio");
 	var home = document.getElementById("home");
-	var array = [music, myStory, social, contact, portfolio];
+	var array = [music, myStory, social, contact, portfolio, home];
   $( window ).resize(function() {
  	  layoutFunction();
 	});
-
   $(document).scroll(function(){
   	for (var i = 0; i < array.length; i++) {
   		menusScroll(array[i])
-	  	}
-	  if($(this).scrollTop() > 0){   
-	    $('.elieNav').removeClass("topNav").addClass("scrollNav");   
-	    $(".navbar-brand").removeClass("brandHide").addClass("brandShow");
-	   } 
-    else { 
-      $('.elieNav').removeClass("scrollNav").addClass("topNav");
-      $(".navbar-brand").removeClass("brandShow").addClass("brandHide");
-      $(".nav").find(".active").removeClass("active");
-  	}
+	  }
+    scrollFunction()
 	});
-   $('a[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
   	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 		  var target = $(this.hash);
 		  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
